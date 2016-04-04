9f9b40e304383c442fdf7691a7bfdb10d0222493
andrijadjordjevic1995 <andrija.djordjevic1995@gmail.com>
2016-04-04 16:50:32 +0200

    Napisana javadoc dokumentacija klase TwitterPoruka, njenih atributa i metoda

--- Twitter/src/com/twitter/poruke/TwitterPoruka.java
+++ Twitter/src/com/twitter/poruke/TwitterPoruka.java
@@ -1,28 +1,72 @@
 package com.twitter.poruke;
 
-
+/**
+ * 
+ * @author Andrija Djordjevic
+ * @version 0.1 prva verzija klase TwitterPoruka
+ *
+ */
 public class TwitterPoruka {
+	/**
+	 Cuva se ime korisnika
+	 */
 	private String korisnik;
+	
+	/**
+	 * Cuva se sadrzaj poruke
+	 */
 	private String poruka;
+
+	/**
+	 * 
+	 * @return Vraca String koji sadrzi ime korisnika
+	 */
 	public String getKorisnik() {
-	return korisnik;
+		return korisnik;
 	}
+
+	/**
+	 * 
+	 * @param korisnik sadrzi ime korisnika
+	 * 
+	 * Metoda postavlja vrednost atributa korisnik na prosledjenu vrednost
+	 * 
+	 * @throws RuntimeException ako se metodi prosledi null ili prazan String
+	 */
 	public void setKorisnik(String korisnik) {
-	if (korisnik==null || korisnik == "")
-	throw new RuntimeException(
-	"Ime korisnika mora biti uneto");
-	this.korisnik = korisnik;
+		if (korisnik == null || korisnik == "")
+			throw new RuntimeException("Ime korisnika mora biti uneto");
+		this.korisnik = korisnik;
 	}
+
+	/**
+	 * 
+	 * @return Vraca String koji sadrzi poruku
+	 */
 	public String getPoruka() {
-	return "poruka";
+		return "poruka";
 	}
+
+	/**
+	 * 
+	 * @param poruka sadrzi poruku koju je poslao korisnik
+	 * 
+	 * @throws RuntimeException u slucaju da je prosledjena poruka null ili ima vise od 140 znakova
+	 * 
+	 * Metoda dodeljuje atributu poruka prosledjenu vrednost (vrednost parametra poruka)
+	 */
 	public void setPoruka(String poruka) {
-	if (poruka==null || this.poruka.length()>140)
-	throw new RuntimeException(
-	"Poruka mora biti uneta i mora imati najvise 140 znakova");
-	this.poruka = poruka;
+		if (poruka == null || this.poruka.length() > 140)
+			throw new RuntimeException("Poruka mora biti uneta i mora imati najvise 140 znakova");
+		this.poruka = poruka;
 	}
-	public String toString(){
-	return "KORISNIK:"+korisnik+" PORUKA:"+poruka;
+
+	/**
+	 * Redefinisana metoda toString
+	 * 
+	 * @return String koji sadrzi ime korisnika i poruku
+	 */
+	public String toString() {
+		return "KORISNIK:" + korisnik + " PORUKA:" + poruka;
 	}
 }
